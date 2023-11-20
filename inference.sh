8af1e62fa186f0cc64ae8c2d1eeaa73656e5548e
Sang-Hoon Lee <56749640+sh-lee-prml@users.noreply.github.com>
2023-11-20 15:17:33 +0900

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,10 +1,10 @@
 # --ckpt "logs/hierspeechpp_libritts460/hierspeechpp_lt460_ckpt.pth" \ LibriTTS-460
 # --ckpt "logs/hierspeechpp_libritts960/hierspeechpp_lt960_ckpt.pth" \ LibriTTS-960
 # --ckpt "logs/hierspeechpp_eng_kor/hierspeechpp_v1_ckpt.pth" \ Large_v1 epoch 60 (paper version)
-# --ckpt "logs/hierspeechpp_eng_kor/hierspeechpp_v2_ckpt.pth" \ Large_v2 epoch 110 (08. Nov. 2023)
+# --ckpt "logs/hierspeechpp_eng_kor/hierspeechpp_v1.1_ckpt.pth" \ Large_v1.1 epoch 200 (20. Nov. 2023)
 
 CUDA_VISIBLE_DEVICES=0 python3 inference.py \
-                --ckpt "logs/hierspeechpp_eng_kor/hierspeechpp_v2_ckpt.pth" \
+                --ckpt "logs/hierspeechpp_eng_kor/hierspeechpp_v1.1_ckpt.pth" \
                 --ckpt_text2w2v "logs/ttv_libritts_v1/ttv_lt960_ckpt.pth" \
                 --output_dir "tts_results_eng_kor_v2" \
                 --noise_scale_vc "0.333" \
